65b4efefc0f8e7bc74189570cc04ce86af5310ff
ITzhangjm@gmail.com <2581314zjm>
2017-12-29 17:54:30 +0800

    i18n

---
+++ springboot-i18n/src/main/java/com/jimzhang/utils/LocaleMessageSourceUtil.java
@@ -0,0 +1,46 @@
+package com.jimzhang.utils;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Locale;
+
+/**
+ * @author jimzhang
+ * @version V1.0.0
+ * @description
+ * @home <>https://segmentfault.com/u/itzhangjm</>
+ * @date 2017-12-29 17:43
+ */
+@Component
+public class LocaleMessageSourceUtil {
+    @Resource
+    private MessageSource messageSource;
+
+    public String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    /**
+     * @param code ：对应messages配置的key.
+     * @param args : 数组参数.
+     * @return
+     */
+    public String getMessage(String code, Object[] args) {
+        return getMessage(code, args, "");
+    }
+
+    /**
+     * @param code           ：对应messages配置的key.
+     * @param args           : 数组参数.
+     * @param defaultMessage : 没有设置key的时候的默认值.
+     * @return
+     */
+    public String getMessage(String code, Object[] args, String defaultMessage) {
+        //这里使用比较方便的方法，不依赖request.
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(code, args, defaultMessage, locale);
+    }
+}
