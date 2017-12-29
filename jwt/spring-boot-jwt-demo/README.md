4bb11ef228e28bd275bba03495018967c6521c09
ITzhangjm@gmail.com <2581314zjm>
2017-12-29 14:10:11 +0800

    add jwt

---
+++ jwt/spring-boot-jwt-demo/README.md
@@ -0,0 +1,18 @@
+
+Simple demo with only 3 classes  
+
+#### Please run example in *basic* folder;  
+#### Then try to look at complete version in *complete* folder
+
+github：https://github.com/ZhongjunTian/spring-boot-jwt-demo
+
+简书：https://www.jianshu.com/p/e62af4a217eb
+
+1.basic
+
+使用 JWT 实现认证 Authentication
+
+2.complete
+
+ 使用 JWT 实现 完整版的授权机制 Authorization
+
---
+++ jwt/spring-boot-jwt-demo/basic/src/main/java/basic/JwtUtil.java
@@ -0,0 +1,51 @@
+package basic;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    // 1 hour
+    static final long EXPIRATION_TIME = 3600_000;
+    static final String SECRET = "ThisIsASecret";
+    static final String TOKEN_PREFIX = "Bearer";
+    static final String HEADER_STRING = "Authorization";
+
+    public static String generateToken(String username) {
+        HashMap<String, Object> map = new HashMap<>();
+        //you can put any data in the map
+        map.put("username", username);
+        String jwt = Jwts.builder()
+                .setClaims(map)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        return jwt;
+    }
+
+    public static String validateToken(String token) {
+        if (token != null) {
+            // parse the token.
+            Map<String,Object> body = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                    .getBody();
+            String username = (String) (body.get("username"));
+            if(username == null || username.isEmpty())
+                throw new TokenValidationException("Wrong token without username");
+            else
+                return username;
+        }else{
+            throw new TokenValidationException("Missing token");
+        }
+    }
+
+    static class TokenValidationException extends RuntimeException {
+        public TokenValidationException(String msg) {
+            super(msg);
+        }
+    }
+}
