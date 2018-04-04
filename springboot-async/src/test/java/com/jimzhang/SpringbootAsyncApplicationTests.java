76fb13a0fcb5fce2f54d16a717290459ca147692
zhangjinmiao <itzhangjm@gmail.com>
2018-04-04 11:18:27 +0800

    async thread

---
+++ springboot-async/src/test/java/com/jimzhang/SpringbootAsyncApplicationTests.java
@@ -0,0 +1,27 @@
+package com.jimzhang;
+
+import com.jimzhang.async.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAsyncApplicationTests {
+
+	@Autowired
+	private Task task;
+
+	@Test
+	public void test() throws Exception {
+
+		task.doTaskOne();
+		task.doTaskTwo();
+		task.doTaskThree();
+
+		Thread.currentThread().join();
+	}
+
+}
